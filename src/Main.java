b813dd313483444be750ee2740c84aa9b4efc0b4
Aidar <raidar920357@gmail.com>
2024-11-14 23:48:26 +0500

    HW done

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,30 @@
 public class Main {
 
-public static void main(String[] args){
+    public static void main(String[] args) {
+
+        System.out.println("Задание 1");
+
+        String firstName = "Айдар";
+        String middleName = "Азаматович";
+        String lastName = "Сакаев";
+        String fullName = lastName + " " + firstName + " " + middleName;
+
+        System.out.println(fullName);
+
+        System.out.println();
+        System.out.println("Задание 2");
+
+        fullName = "Иванов Иван Иванович";
+        System.out.println(fullName.toUpperCase());
+
+        System.out.println();
+        System.out.println("Задание 3");
+
+        fullName = "Иванов Семён Семёнович";
+        String fullNameTrue = fullName.replace("ё", "е");
+        System.out.println(fullNameTrue);
 
 
-}
+    }
 
 }
